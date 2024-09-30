e6a489083c0ea88cab9f8653388ef1688d465bd3
Anrich Potgieter <anrichp@gmail.com>
2024-09-30 18:46:33 +0000

    increment version

--- version.php
+++ version.php
@@ -24,6 +24,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_greetings';
 $plugin->release = '0.1.0';
-$plugin->version = 2022021600;
+$plugin->version = 2024093000;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
